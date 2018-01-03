f06d170c03c1d92d30173701a1b5c9cae224a7ea
trabing <kenso.trabing@bloomwebsite.com>
2018-01-03 03:20:16 -0500

    Polish runtime

--- wizard/core/wrappers/digitalocean.py
+++ wizard/core/wrappers/digitalocean.py
@@ -5,11 +5,11 @@ import re
 
 
 def valid_hostname(hostname):
-    if 0 < len(hostname) < 251: # Logic: 255-len('-999') # TODO Modularize this hard-coded upper-limit.
+    if 0 < len(hostname) < 252: # Logic: difference(255-len('-99'))              # TODO Modularize this hard-coded upper-limit.
         if re.match('^[a-zA-Z0-9]+(-[a-zA-Z0-9]+)*$', hostname):
             return hostname
     else:
-        valid_hostname(input('ValueError: Invalid hostname. Try again: ')) # Raise an actual ValueError.
+        valid_hostname(input('ValueError: Invalid hostname. Try again: '))       # TODO Raise an actual ValueError.
 
 def valid_ran_code(ran_code):
     networks = ['nyc1', 'nyc2', 'nyc3', \
@@ -23,7 +23,7 @@ def valid_ran_code(ran_code):
     if ran_code.lower() in networks:
         return ran_code
     else:
-        valid_ran_code(input('ValueError: Invalid ran_code. Try again: ')) # Raise an actual ValueError.
+        valid_ran_code(input('ValueError: Invalid ran_code. Try again: '))       # TODO Raise an actual ValueError.
 
 def valid_ram_size(ram_size):
     memories = ['512mb',                           \
@@ -32,7 +32,7 @@ def valid_ram_size(ram_size):
     if ram_size.lower() in memories:
         return ram_size
     else:
-        valid_ram_size(input('ValueError: Invalid ram_size. Try again: ')) # Raise an actual ValueError.
+        valid_ram_size(input('ValueError: Invalid ram_size. Try again: '))       # TODO Raise an actual ValueError.
 
 def valid_os_image(os_image):
     machines = ['centos-6-x32', 'centos-6-x64',             \
@@ -53,19 +53,19 @@ def valid_os_image(os_image):
     if os_image.lower() in machines:
         return os_image
     else:
-        valid_os_image(input('ValueError: Invalid os_image. Try again: ')) # Raise an actual ValueError.
+        valid_os_image(input('ValueError: Invalid os_image. Try again: '))       # TODO Raise an actual ValueError.
 
 def valid_pat_path(pat_path):
     try:
         return open('{pat_path}'.format(pat_path=pat_path), 'r').read()
     except IOError:
-        valid_pat_path(input('IOError: Invalid PAT path. Try again: ')) # TODO Raise an actual IOError.
+        valid_pat_path(input('IOError: Invalid PAT path. Try again: '))          # TODO Raise an actual IOError.
 
 def valid_vm_count(vm_count):
-    if 0 < int(vm_count) < 7: # TODO Modularize this hard-coded upper-limit.
+    if 0 < int(vm_count) < 7:                                                    # TODO Modularize this hard-coded upper-limit.
         return vm_count
     else:
-        valid_vm_count(input('ValueError: Invalid VM count. Try again: ')) # TODO Raise an actual ValueError.
+        valid_vm_count(input('ValueError: Invalid VM count. Try again: '))       # TODO Raise an actual ValueError.
 
 def create_cluster():
     endpoint = 'https://api.digitalocean.com/v2/droplets'
@@ -78,17 +78,26 @@ def create_cluster():
     if vm_count < 2:
         api_data['name'] = hostname
     else:
-        api_data['names'] = ['{hostname} {_}'.format(hostname=hostname, _=_) \
-                                .replace(' ', '-')                           \
+        api_data['names'] = ['{hostname} {_}'              \
+                                .format(hostname=hostname, \
+                                        _=_)               \
+                                .replace(' ', '-')         \
                                 for _ in range(vm_count)]
     api_data['region'] = str(valid_ran_code(input('RAN code: ')))
     api_data['size']   = str(valid_ram_size(input('RAM size: ')))
     api_data['image']  = str(valid_os_image(input('OS image: ')))
-    endstate = 'curl -X POST "{endpoint}" \
-                -d "{api_data}"           \
-                -H "{a_header}"           \
-                -H "{c_header}"'.format(endpoint=endpoint,             \
-                                        api_data=json.dumps(api_data), \
-                                        a_header=a_header.strip(),     \
-                                        c_header=c_header)
+    # api_data['ssh_keys'] =                                                     # FIXME
+    # api_data['backups'] =                                                      # FIXME
+    # api_data['ipv6'] =                                                         # FIXME
+    # api_data['user_data'] =                                                    # FIXME
+    # api_data['private_networking'] =                                           # FIXME
+    # api_data['tags'] =                                                         # FIXME
+    endstate = 'curl -X POST "{endpoint}"              \
+                -d \'{api_data}\'                      \
+                -H "{a_header}"                        \
+                -H "{c_header}"'                       \
+                .format(endpoint=endpoint,             \
+                        api_data=json.dumps(api_data), \
+                        a_header=a_header.strip(),     \
+                        c_header=c_header)
     return re.sub(' +', ' ', endstate)
