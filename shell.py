bcd434987208065f9fffcc7a9488df125050c073
trabing <kenso.trabing@bloomwebsite.com>
2018-01-03 02:08:57 -0500

    Write runtime

--- shell.py
+++ shell.py
@@ -1,3 +1,25 @@
 #!/usr/bin/env python3
 
-# virtualenv --no-site-packages venv
+import json
+import os
+
+from wizard.core.wrappers import digitalocean
+
+
+def run():
+    aws_lightsail = ['awsl', 'aws lightsail']
+    digital_ocean = ['do', 'digital ocean']
+    iaas_platform = aws_lightsail + digital_ocean
+    vendor_choice = input('Vendor: ').lower()
+    if vendor_choice in iaas_platform:
+        if vendor_choice in aws_lightsail:
+            pass # TODO
+        elif vendor_choice in digital_ocean:
+            print(digitalocean.create_cluster())
+            # os.system(digitalocean.create_cluster())
+    else:
+        pass # TODO
+
+
+if __name__ == '__main__':
+    run()
